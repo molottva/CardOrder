8349e7af984a23ce354ad74793d5f89e7fbfc55d
molottva <molottva@list.ru>
2022-06-08 15:49:20 +0300

    CI test pass

--- src/test/java/ru/netology/CallBackTest.java
+++ src/test/java/ru/netology/CallBackTest.java
@@ -24,7 +24,7 @@ public class CallBackTest {
         form.$("[data-test-id=phone] input").setValue("+79211234567");
         form.$("[data-test-id=agreement]").click();
         form.$("button.button").click();
-        $("[data-test-id=order-success]").shouldNotHave(
+        $("[data-test-id=order-success]").shouldHave(
                 exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
